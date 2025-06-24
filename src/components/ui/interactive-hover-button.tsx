dbdcea9d1a9acc8842adaab71b53af5c8896320b
mailbobg <wangbo77@hotmail.com>
2025-06-24 10:43:13 +0800

    feat: increase interactive hover button size by 30%

--- src/components/ui/interactive-hover-button.tsx
+++ src/components/ui/interactive-hover-button.tsx
@@ -15,7 +15,7 @@ const InteractiveHoverButton = React.forwardRef<
     <button
       ref={ref}
       className={cn(
-        "group relative w-32 cursor-pointer overflow-hidden rounded-full border bg-background p-2 text-center font-semibold",
+        "group relative w-44 cursor-pointer overflow-hidden rounded-full border bg-background p-3 text-center font-semibold text-base",
         className,
       )}
       {...props}
